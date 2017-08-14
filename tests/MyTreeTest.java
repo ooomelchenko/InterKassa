9b28c1321e7466f8ebaa0209e5be10fbcebb7ca0
alexandr_omelchenko <ooomelchenko@gmail.com>
2017-08-14 12:18:01 +0300

    update MyTreeTest

--- tests/MyTreeTest.java
+++ tests/MyTreeTest.java
@@ -12,11 +12,11 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class MyTreeTest {
     private MyTree myTree;
     private LeafList leafList;
-    Leaf leaf1 = new Leaf(1);
-    Leaf leaf2 = new Leaf(2);
-    Leaf leaf3 = new Leaf(3);
-    Leaf leaf4 = new Leaf(4);
-    Leaf leaf5 = new Leaf(5);
+    Leaf leaf1 ;
+    Leaf leaf2 ;
+    Leaf leaf3 ;
+    Leaf leaf4 ;
+    Leaf leaf5 ;
 
     @BeforeEach
     void init() {
